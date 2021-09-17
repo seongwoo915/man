5292e916b18a92099208928d0d707add2d61b5f6
user <user@DESKTOP-G91HVQ7>
2021-09-17 16:46:17 +0900

    fsg

---
+++ ch03/src/ch02/ArithmeticExam3.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class ArithmeticExam3 {
+	
+	public static void main(String[] args) {
+		int input = 10;
+		//¼ýÀÚ¸¦ ÀÔ·Â¹Þ¾Æ¼­ Â¦¼ö/È¦¼ö
+		if(input % 2 == 0){//Â¦¼ö
+			System.out.println("Â¦¼ö");
+		}else {
+			System.out.println("È¦¼ö");
+		}
+		
+	}
+}
